d63e0af88bfaadc0f8aa45e9448a7cb5f9715507
Reid Brown <reidbrown23@gmail.com>
2024-09-20 08:52:43 -0400

    Updates

--- components/navBar.js
+++ components/navBar.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const NavBar = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.navBar}>
       <MaterialIcons name="home-filled" size={32} color="white" />
+      <MaterialIcons name="search" size={32} color="white" />
+      <MaterialCommunityIcons name="bell-outline" size={32} color="white" />
+      <TouchableOpacity onPress={() => navigation.navigate("Account")}>
+        <MaterialCommunityIcons name="account" size={32} color="white" />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -16,7 +24,7 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: "rgba(0, 0, 0, 0.7)", // Black color with 50% opacity
     flexDirection: "row",
-    justifyContent: "space-evenly",
+    justifyContent: "space-around",
     paddingTop: 10,
   },
 });
