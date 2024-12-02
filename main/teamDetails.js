ee7240e6f3e3babc638282e34893f17e0543ba1f
Reid Brown <reidbrown23@gmail.com>
2024-12-02 15:00:14 -0500

    Updates

--- main/teamDetails.js
+++ main/teamDetails.js
@@ -1,44 +1,87 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import { useRoute } from "@react-navigation/native";
 
 export default function TeamDetails() {
   const route = useRoute();
   const { teamId, sportName, league } = route.params; // Get teamId from route parameters
   const [teamData, setTeamData] = useState(null);
-  const [logo, setLogo] = useState(null);
-  const [name, setName] = useState(null);
-  const [record, setRecord] = useState(null);
-  const [rank, setRank] = useState(null);
-
+  const [scheduleData, setScheduleData] = useState(null);
   const fetchTeamDetails = async () => {
     const response = await fetch(
       `https://site.api.espn.com/apis/site/v2/sports/${sportName}/${league}/teams/${teamId}`
     );
+    console.log(response.url);
     const data = await response.json();
+    setTeamData(data);
+  };
 
-    const logo = data.team.logos[1].href;
-    const name = data.team.displayName;
-    const record = data.team.record.items[0].summary;
-    const rank = data.team.rank;
+  const fetchTeamSchedule = async () => {
+    const response = await fetch(
+      `https://site.api.espn.com/apis/site/v2/sports/${sportName}/${league}/teams/${teamId}/schedule`
+    );
+    console.log(response.url);
 
-    setLogo(logo);
-    setName(name);
-    setRecord(record);
-    setRank(rank);
+    const data = await response.json();
+    setScheduleData(data);
+    // Handle schedule data if needed
   };
 
   useEffect(() => {
     fetchTeamDetails();
+    fetchTeamSchedule();
   }, []);
+
+  // Check if teamData is available before accessing its properties
+  if (!teamData) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator color="white" />
+      </View>
+    );
+  }
+
+  const logo = teamData.team.logos[1].href;
+  const shortDisplayName = teamData.team.shortDisplayName;
+  const name = teamData.team.name;
+  const record = teamData.team.record.items[0].summary;
+  const rank = teamData.team.rank;
 
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <Image style={styles.logo} source={{ uri: logo }} />
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.record}>{record}</Text>
-        <Text style={styles.rank}>Rank: #{rank}</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Image style={styles.logo} source={{ uri: logo }} />
+          <View
+            style={{
+              flexDirection: "column",
+              alignItems: "center",
+              marginLeft: 10,
+            }}
+          >
+            <Text style={styles.name}>
+              {rank ? `#${rank} ` : ""}
+              {shortDisplayName}
+            </Text>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.record}>{record}</Text>
+          </View>
+        </View>
+        <Text style={styles.schedule}>Schedule</Text>
+        <View style={styles.scheduleContainer}></View>
       </ScrollView>
     </View>
   );
@@ -56,27 +99,31 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
   },
   logo: {
-    width: 150,
-    height: 150,
+    width: 75,
+    height: 75,
     marginBottom: 20,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: "white",
   },
   name: {
-    fontSize: 28,
+    fontSize: 20,
     fontWeight: "bold",
     color: "white",
     marginBottom: 10,
   },
   record: {
-    fontSize: 20,
+    fontSize: 18,
     color: "white",
-    marginBottom: 5,
+    marginBottom: 10,
   },
-  rank: {
-    fontSize: 20,
+  schedule: {
+    fontSize: 24,
     color: "white",
-    marginBottom: 20,
+    marginBottom: 10,
+    fontWeight: "bold",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
   },
 });
